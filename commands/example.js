d25ff80d527e9f5e570ebdccc452a194f6962787
Brncray <57870234+Brncray@users.noreply.github.com>
2024-02-26 19:21:34 -0500

    Removed comments from prefab

--- commands/example.js
+++ commands/example.js
@@ -1,20 +1,20 @@
 /**@type {import("../bot.js").Command} */
 export const data = {
-  name: "hello-world",
+  name: "",
   type: 1, // u got 3 types, 1 is reg cmd, 2 is msg app, 3 is user app
-  description: "yooo description",
+  description: "",
   options: [
     {
       type: 3, // STRING Type
       name: "query",
-      description: "search smth here!",
+      description: "search ",
       required: false,
-      choices: [{ name: "YOOOOO", value: "" }],
+      choices: [{ name: "", value: "" }],
       autocomplete: false,
     },
   ],
-  dm_permission: false, // ensures that the command cannot be used inside of dms
-  default_member_permissions: 0, // u can use default member permission to lock cmds to certain permission levels, ex administrator, u can use permissionbitfield to get one if u cant via discord docs
+  dm_permission: false, 
+  default_member_permissions: 0, 
 };
 /**
  *
@@ -22,20 +22,19 @@ export const data = {
  * @param {import("../bot.js").Bot} client
  */
 export async function execute(interaction, client) {
-  await interaction.deferReply({ ephemeral: true }); // must define whether its true or false for ephemeral, if u leave it blank wo ephemeral where it looks like: await interaction.deferReply(); then itll autoresort to no ephemeral
-  const query = interaction.options.getString("query"); // for the true statement put inside of that constant, u only put true after the name to ensure its requirement is true, if not u js remove it
+  await interaction.deferReply({ ephemeral: true }); 
+  const query = interaction.options.getString("query"); 
 
-  /**@type {import("discord.js").APIEmbed[]} */ // u use [] if its an array, if its a object {}, then u js leave it wo array, ex APIEmbed
+  /**@type {import("discord.js").APIEmbed[]} */ 
   const e = [
     {
       title: "Hello World",
-      description: "hello world description", // if u use `` to use a function or constant u can use \` to make it in that black chat box, u dont need to use it if you have '' or "" instead
+      description: "hello world description", 
       color: client.settings.color,
     },
   ];
-  await interaction.reply({ embeds: e, content: "hello" }); // u can use allowedMentions, ill provide the shit in a sec
+  await interaction.reply({ embeds: e, content: "hello" });
 
-  // is there auto_defer in djs?
-  // python, if you used auto_defer emhemeral would only work in certain cases | yea ephemeral works for interaction.reply, u do have a defer options lemme show rq
-  await interaction.editReply({ embeds: e }); // ephemeral hides message, if u have it as {} for ur embed instead of an array then u put {constnamehere} where embeds: is, ex. embeds: {constnamehere}
+
+  await interaction.editReply({ embeds: e }); 
 }
