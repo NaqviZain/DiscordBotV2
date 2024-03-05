d59716a63c0ec2a033d99d9df8b79ac68e2938d7
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-05 09:58:41 -0500

    Update reinvites.cmd.js

--- commands/staff/reinvites.cmd.js
+++ commands/staff/reinvites.cmd.js
@@ -63,7 +63,7 @@ export async function execute(interaction, client) {
   ];
 
   const msg = await interaction.channel?.send({
-    content: "@everyone",
+    content: "@here",
     embeds: resp_embed,
     allowedMentions: { parse: ["everyone"] },
   });
