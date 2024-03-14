200a7221f3adc00eb0bd0ff488e646497e17a176
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-14 13:05:08 -0400

    Create willay.cmd.js

---
+++ commands/willay.cmd.js
@@ -0,0 +1,26 @@
+/**@type {import("../bot.js").Command} */
+
+import Profile from "../schemas/profile_schema.js";
+
+export const data = {
+  name: "willay",
+  type: 1, // u got 3 types, 1 is reg cmd, 2 is msg app, 3 is user app
+  description: "willay",
+  dm_permission: false, // ensures that the command cannot be used inside of dms
+  default_member_permissions: 0, // u can use default member permission to lock cmds to certain permission levels, ex administrator, u can use permissionbitfield to get one if u cant via discord docs
+};
+/**
+ *
+ * @param {import("discord.js").ChatInputCommandInteraction<'cached'>} interaction
+ * @param {import("../bot.js").Bot} client
+ */
+export async function execute(interaction, client) {
+
+  if for {
+    while (1==1){
+      penis = 69420; 
+  }
+  }
+}
+await interaction.reply({content:"WILLAY PENIS"}); 
+}
