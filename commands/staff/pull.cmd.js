54f9936596d5b13fb89c249d7fb324de2ff64ddb
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-15 15:46:30 +0000

    fixed pull

--- commands/staff/pull.cmd.js
+++ commands/staff/pull.cmd.js
@@ -1,38 +1,44 @@
-import { spawn } from "node:child_process"; 
+import { exec } from "node:child_process";
 /** @type {import("../../bot.js").Command} */
 export const data = {
-    name: "pull",
-    type: 1,
-    description: "Pull & Restart",
-    dm_permission: false,
-    default_member_permissions: 0
-  };
-  
-  /**
-   *
-   * @param {import("discord.js").ChatInputCommandInteraction<'cached'>} interaction
-   * @param {import("../../bot.js").Bot} client
-   */
-  export async function execute(interaction, client) {
-    await interaction.deferReply({ ephemeral: false });
-    if (interaction.member.id != "539213950688952320") {
-      return interaction.reply({
-        content: "You do not have the proper permissions to use this command.",
-        ephemeral: true
+  name: "pull",
+  type: 1,
+  description: "Pull & Restart",
+  dm_permission: false,
+  default_member_permissions: 0,
+};
+
+/**
+ *
+ * @param {import("discord.js").ChatInputCommandInteraction<'cached'>} interaction
+ * @param {import("../../bot.js").Bot} client
+ */
+export async function execute(interaction, client) {
+  await interaction.deferReply({ ephemeral: false });
+  if (interaction.member.id != "539213950688952320") {
+    return interaction.reply({
+      content: "You do not have the proper permissions to use this command.",
+      ephemeral: true,
+    });
+  }
+
+  await interaction.editReply({
+    content: `Pulling...`,
+    ephemeral: false,
+  });
+  await exec("git pull origin main && npm install", async (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return interaction.editReply({
+        content: `Error while pulling`,
+        ephemeral: false,
       });
     }
-
-    try {
-        const command = "git pull origin main && npm install && pm2 restart index";
-        command.stdout.on('data', output => {
-            // the output data is captured and printed in the callback
-            console.log("Output: ", output.toString())
-        })
-    } catch (e) {
-        console.log(e); 
-        return interaction.reply({
-            content: `Error while pulling & restarting`,
-            ephemeral: false
-          });
-    }
-  }
+    console.log(stdout);
+    console.log(stderr);
+    await interaction.editReply({
+      content: `Pulled...`,
+      ephemeral: false,
+    });
+  });
+}
